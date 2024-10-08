d487a5e5ac1a5907a6f2d685c236882c45b25d2d
NicolasBarutti <nicolasbarutti2003@gmail.com>
2024-10-08 11:47:40 -0300

    Aula 2 next

---
+++ next-crud2/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grow p-7">
+      <h1 className="text-center text-3xl text-indigo-600 font-bold">Home</h1>
+    </div>
+  );
+}
---
+++ next-crud2/src/app/produtos/produto/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { TipoProduto } from "@/types"
+import { useState } from "react"
+
+export default function Produto({params}:{params:{id:number}}){
+
+    const [produto, setProduto] = useState<TipoProduto>(gi)
+
+    return(
+        <main></main>
+    )
+}
